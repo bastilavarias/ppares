9b0ba3338aa37b36e642d1e7226136ee36d661dd
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2024-10-24 20:15:42 +0800

    WIP

---
+++ web/lib/utils.ts
@@ -0,0 +1,10 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export function toPrice(price) {
+    return `₱${price.toFixed(2)} PHP`;
+}
